d15b49a20ce457e84e82451d18e436a33f3faf83
MahdiHT <mht0440232@gamil.com>
2020-05-10 14:42:15 +0430

    test poppeteer

---
+++ test.js
@@ -0,0 +1,9 @@
+const puppeteer = require('puppeteer');
+async function run () {
+    const browser = await puppeteer.launch();
+    const page = await browser.newPage();
+    await page.goto("https://najva.com");
+    await page.screenshot({path: 'screenshot.png'});
+    browser.close();
+}
+run();
